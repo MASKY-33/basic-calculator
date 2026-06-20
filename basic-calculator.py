dec5ee4e49adf9bbfd9f39f20737171f95583b11
MASKY-33 <masky.somebody33@gmail.com>
2026-06-20 15:41:42 +0200

    Create basic-calculator.py

---
+++ basic-calculator.py
@@ -0,0 +1,13 @@
+## This Program:
+# - asks 2 numbers
+# - prints their "sum (add up)" both Datatypes
+# - prints their "difference (substract)" both Datatypes
+# - prints their "product (multiply)" both Datatypes
+
+
+first_number = int(input("Give a number: "))
+sec_number = int(input("Give one more number: "))
+ 
+print(first_number + sec_number)
+print(first_number - sec_number)
+print(first_number * sec_number)
